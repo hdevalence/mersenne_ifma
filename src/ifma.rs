ad71db0ade7ececbbe11ce1b3dc5999dbcf7b033
Henry de Valence <hdevalence@hdevalence.ca>
2018-11-29 13:17:23 -0800

    Add docs

--- src/ifma.rs
+++ src/ifma.rs
@@ -1,5 +1,16 @@
+//! Definitions of (256-bit wide) IFMA intrinsics.
+//!
+//! Because Rust doesn't have support for AVX-512 yet, we manually
+//! define our own intrinsics that lower to 256-bit IFMA instructions.
+//! This doesn't really have a downside compared to using 512-bit
+//! vectors, because on the only available Cannonlake processor, the
+//! i3-8121U, executes IFMA at 256-bit wide anyways, and this nicely
+//! matches the 4-way parallel Edwards formulas.
+
 use packed_simd::u64x4;
 
+// The `link_name`s below are pulled out of LLVM tablegen, have
+// changed in the past, and might change again in the future.
 #[allow(improper_ctypes)]
 extern "C" {
     #[link_name = "llvm.x86.avx512.vpmadd52l.uq.256"]
@@ -8,17 +19,28 @@ extern "C" {
     fn madd52hi_intrin(z: u64x4, x: u64x4, y: u64x4) -> u64x4;
 }
 
+/// A safe wrapper around `vpmadd52luq`.
+///
+/// The intrinsic itself is unsafe because it could generate SIGILL,
+/// but this crate can't be compiled except for IFMA targets.
 #[inline]
 pub fn madd52lo(z: u64x4, x: u64x4, y: u64x4) -> u64x4 {
-    madd52lo_intrin(z, x, y)
+    unsafe { madd52lo_intrin(z, x, y) }
 }
+
+/// A safe wrapper around `vpmadd52huq`.
+///
+/// The intrinsic itself is unsafe because it could generate SIGILL,
+/// but this crate can't be compiled except for IFMA targets.
 #[inline]
 pub fn madd52hi(z: u64x4, x: u64x4, y: u64x4) -> u64x4 {
-    madd52hi_intrin(z, x, y)
+    unsafe { madd52hi_intrin(z, x, y) }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_intrinsics() {
         let a = u64x4::new(1, 2, 3, 4);
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,14 @@
 //! prime `2**127 -1`.
 
 #![feature(simd_ffi, link_llvm_intrinsics)]
+#![deny(missing_docs)]
+#![cfg(not(target_feature = "avx512ifma"))]
+compile_error!("This crate requires AVX512-IFMA");
 
+// The `packed_simd` crate contains what would have been the Rust SIMD
+// code, except that it was decided to use untyped Intel __m256i
+// bag-of-bits types instead of nice u64x4 types with arithmetic
+// operations, so we use it instead of core::arch.
 extern crate packed_simd;
 
 mod ifma;
