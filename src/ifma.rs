dddf64f6710f7c286ff54c70a926d2807b0d8bd6
Henry de Valence <hdevalence@hdevalence.ca>
2018-11-29 13:40:10 -0800

    fixup! fixup! Add docs

--- src/ifma.rs
+++ src/ifma.rs
@@ -44,13 +44,11 @@ mod tests {
     #[test]
     fn test_intrinsics() {
         let a = u64x4::new(1, 2, 3, 4);
-        let b = u64x4::splat((1 << 52) + 3);
+        let b = u64x4::splat(3);
         let c = u64x4::new(5, 6, 7, 8);
 
         let x = madd52lo(a, b, c);
-        let y = madd52hi(a, b, c);
 
         assert_eq!(x, u64x4::new(1 + 3 * 5, 2 + 3 * 6, 3 + 3 * 7, 4 + 3 * 8));
-        assert_eq!(y, u64x4::new(1 + 1 * 5, 2 + 1 * 6, 3 + 1 * 7, 4 + 1 * 8));
     }
 }
