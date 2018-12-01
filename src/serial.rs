d73feb7d5b7eed49cd3ab560180550ab9dd9df6c
Henry de Valence <hdevalence@hdevalence.ca>
2018-12-01 10:36:34 -0800

    wip changes

--- src/serial.rs
+++ src/serial.rs
@@ -1,1 +1,13 @@
+//! Serial arithmetic.
+//!
+//! This code implements prime-field and extension-field arithmetic
+//! using `u128`s. Speed is not the highest priority, because the idea
+//! is that the bulk of the work will be done using the vectorized
+//! implementation.
+//!
+//!
+
+
+pub struct F127(u128);
+
 
--- src/vector.rs
+++ src/vector.rs
@@ -26,50 +26,92 @@
 //! \\), so that \\( 3r = 129 \\).  This means the weight is spread
 //! evenly across the limbs and the limb boundaries are more closely
 //! aligned with the bitsize of the prime.
-//!
-//!
+
+use core::ops::Mul;
+
+use packed_simd::u64x4;
+
+use crate::ifma::{madd52hi, madd52lo};
 
 pub struct F127x4(u64x4, u64x4, u64x4);
 
+// XXX should this use u128 or F127 ?
 impl From<(u128, u128, u128, u128)> for F127x4 {
     fn from(x: (u128, u128, u128, u128)) -> F127x4 {
         let low_43_bits = (1 << 43) - 1;
 
         F127x4(
             u64x4::new(
-                x.0 & low_43_bits,
-                x.1 & low_43_bits,
-                x.2 & low_43_bits,
-                x.3 & low_43_bits,
+                (x.0 & low_43_bits) as u64,
+                (x.1 & low_43_bits) as u64,
+                (x.2 & low_43_bits) as u64,
+                (x.3 & low_43_bits) as u64,
             ),
             u64x4::new(
-                (x.0 >> 43) & low_43_bits,
-                (x.1 >> 43) & low_43_bits,
-                (x.2 >> 43) & low_43_bits,
-                (x.3 >> 43) & low_43_bits,
+                ((x.0 >> 43) & low_43_bits) as u64,
+                ((x.1 >> 43) & low_43_bits) as u64,
+                ((x.2 >> 43) & low_43_bits) as u64,
+                ((x.3 >> 43) & low_43_bits) as u64,
             ),
             u64x4::new(
-                (x.0 >> 86) & low_43_bits,
-                (x.1 >> 86) & low_43_bits,
-                (x.2 >> 86) & low_43_bits,
-                (x.3 >> 86) & low_43_bits,
+                ((x.0 >> 86) & low_43_bits) as u64,
+                ((x.1 >> 86) & low_43_bits) as u64,
+                ((x.2 >> 86) & low_43_bits) as u64,
+                ((x.3 >> 86) & low_43_bits) as u64,
             ),
         )
     }
 }
 
 impl Into<(u128, u128, u128, u128)> for F127x4 {
-    fn into(x: F127x4) -> (u128, u128, u128, u128) {
+    fn into(self) -> (u128, u128, u128, u128) {
         (
-            x.0.extract(0) + (x.1.extract(0) << 43) + (x.2.extract(0) << 86),
-            x.0.extract(1) + (x.1.extract(1) << 43) + (x.2.extract(1) << 86),
-            x.0.extract(2) + (x.1.extract(2) << 43) + (x.2.extract(2) << 86),
-            x.0.extract(3) + (x.1.extract(3) << 43) + (x.2.extract(3) << 86),
+            (self.0.extract(0) as u128)
+                + ((self.1.extract(0) as u128) << 43)
+                + ((self.2.extract(0) as u128) << 86),
+            (self.0.extract(1) as u128)
+                + ((self.1.extract(1) as u128) << 43)
+                + ((self.2.extract(1) as u128) << 86),
+            (self.0.extract(2) as u128)
+                + ((self.1.extract(2) as u128) << 43)
+                + ((self.2.extract(2) as u128) << 86),
+            (self.0.extract(3) as u128)
+                + ((self.1.extract(3) as u128) << 43)
+                + ((self.2.extract(3) as u128) << 86),
         )
     }
 }
 
-//impl Mul<F127x4> for F127x4(
+impl Mul<F127x4> for F127x4 {
+    type Output = F127x4;
+    #[inline]
+    fn mul(self, other: F127x4) -> F127x4 {
+        use super::ifma::{madd52hi, madd52lo};
+
+        let (x0, y0) = (self.0, other.0);
+        let (x1, y1) = (self.1, other.1);
+        let (x2, y2) = (self.2, other.2);
+
+        let mut z0_a = u64x4::splat(0);
+        let mut z0_b = u64x4::splat(0);
+        let mut z0_c = u64x4::splat(0);
+        let mut z1_a = u64x4::splat(0);
+        let mut z1_b = u64x4::splat(0);
+        let mut z1_c = u64x4::splat(0);
+        let mut z2_a = u64x4::splat(0);
+        let mut z2_b = u64x4::splat(0);
+        let mut z2_c = u64x4::splat(0);
+
+        z0_a = madd52hi(z0_a, x2, y0);
+        z0_a = z0_a << 11;
+        z0_a = madd52lo(z0_a, x0, y0);
+
+        z0_b = madd52lo(z0_b, x2, y1);
+        z0_c = madd52hi(z0_c, x1, y1);
+
+        unimplemented!();
+    }
+}
 
 #[cfg(test)]
 mod tests {
