96d9d186123f74a87012f4f02ec686ccf0787282
Henry de Valence <hdevalence@hdevalence.ca>
2018-12-04 21:56:09 -0800

    first attempt at prime field mul

--- src/vector.rs
+++ src/vector.rs
@@ -92,6 +92,9 @@ impl Mul<F127x4> for F127x4 {
         let (x1, y1) = (self.1, other.1);
         let (x2, y2) = (self.2, other.2);
 
+        // We have 18 multiplications, want 8 independent chains to
+        // saturate the EUs, so split into 9 chains of length 2.
+
         let mut z0_a = u64x4::splat(0);
         let mut z0_b = u64x4::splat(0);
         let mut z0_c = u64x4::splat(0);
@@ -102,14 +105,45 @@ impl Mul<F127x4> for F127x4 {
         let mut z2_b = u64x4::splat(0);
         let mut z2_c = u64x4::splat(0);
 
-        z0_a = madd52hi(z0_a, x2, y0);
-        z0_a = z0_a << 11;
-        z0_a = madd52lo(z0_a, x0, y0);
+        z0_a = madd52hi(z0_a, x2, y0); // 2^11
+        z0_b = madd52lo(z0_b, x2, y1); // 2^2
+        z0_c = madd52hi(z0_c, x1, y1); // 2^11
 
-        z0_b = madd52lo(z0_b, x2, y1);
-        z0_c = madd52hi(z0_c, x1, y1);
+        z1_a = madd52hi(z1_a, x0, y0); // 2^9
+        z1_b = madd52hi(z1_b, x2, y1); // 2^11
+        z1_c = madd52lo(z1_c, x1, y0); // 2^0
 
-        unimplemented!();
+        z2_a = madd52hi(z2_a, x2, y2); // 2^11
+        z2_b = madd52hi(z2_b, x0, y1); // 2^9
+        z2_c = madd52lo(z2_c, x2, y0); // 2^0
+
+        z0_a = z0_a << 11; // 2^11 -> 2^0
+        z1_a = z1_a << 07; // 2^9  -> 2^2
+        z2_a = z2_a << 11; // 2^11 -> 2^0
+
+        z0_a = madd52lo(z0_a, x0, y0); // 2^0
+        z0_b = madd52lo(z0_b, x1, y2); // 2^2
+        z0_c = madd52hi(z0_c, x0, y2); // 2^11
+
+        z1_a = madd52lo(z1_a, x2, y2); // 2^2
+        z1_b = madd52hi(z1_b, x1, y2); // 2^11
+        z1_c = madd52lo(z1_c, x0, y1); // 2^0
+
+        z2_a = madd52lo(z2_a, x0, y2); // 2^0
+        z2_b = madd52hi(z2_b, x1, y0); // 2^9
+        z2_c = madd52lo(z2_c, x1, y1); // 2^0
+
+        let z0 = z0_a + (z0_b << 2) + (z0_c << 11);
+        let z1 = (z1_a << 2) + (z1_b << 11) + z1_c;
+        let z2 = z2_a + (z2_b << 9) + z2_c;
+
+        let c0 = z0 >> 43;
+        let c1 = z1 >> 43;
+        let c2 = z2 >> 43;
+
+        let mask = u64x4::splat((1 << 43) - 1);
+
+        F127x4(((z0 & mask) + (c2 << 2), (z1 & mask) + c0, (z2 & mask) + c1))
     }
 }
 
@@ -131,4 +165,5 @@ mod tests {
 
         assert_eq!(xs, x_vec.into());
     }
+
 }
