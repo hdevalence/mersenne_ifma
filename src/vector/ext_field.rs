181585c4d601ac08640775ad0d133fb2e8b04d3f
Henry de Valence <hdevalence@hdevalence.ca>
2018-12-09 14:35:20 -0800

    add extension field mul

--- src/vector/ext_field.rs
+++ src/vector/ext_field.rs
@@ -28,3 +28,63 @@ impl Into<(ExtF127, ExtF127, ExtF127, ExtF127)> for ExtF127x4 {
         )
     }
 }
+
+use core::ops::{Add, Mul, Neg};
+
+impl Add<ExtF127x4> for ExtF127x4 {
+    type Output = ExtF127x4;
+    #[inline]
+    fn add(self, other: ExtF127x4) -> ExtF127x4 {
+        ExtF127x4(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl Mul<ExtF127x4> for ExtF127x4 {
+    type Output = ExtF127x4;
+    #[inline]
+    fn mul(self, other: ExtF127x4) -> ExtF127x4 {
+        let (a, b) = (self.0, self.1);
+        let (c, d) = (other.0, other.1);
+
+        let ac = a * c;
+        let bd = b * d;
+
+        ExtF127x4(ac + (-bd), (b + (-a)) * (c + (-d)) + ac + bd)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn mul_matches_serial() {
+        let xs = (
+            ExtF127::from((
+                43654918112560223727172090912658261884u128,
+                38188712660835962328561942614081743514u128,
+            )),
+            ExtF127::from((
+                38188712660835962328561942614081743514u128,
+                61331686004747624160469066397670963925u128,
+            )),
+            ExtF127::from((
+                38188712660835962328561942614081743514u128,
+                43654918112560223727172090912658261884u128,
+            )),
+            ExtF127::from((
+                43654918112560223727172090912658261884u128,
+                61331686004747624160469066397670963925u128,
+            )),
+        );
+
+        let x_vec: ExtF127x4 = xs.into();
+
+        let z_vec = x_vec * x_vec;
+
+        let zs: (ExtF127, ExtF127, ExtF127, ExtF127) = z_vec.into();
+
+        assert_eq!(zs.0, xs.0 * xs.0);
+        assert_eq!(zs.1, xs.1 * xs.1);
+        assert_eq!(zs.2, xs.2 * xs.2);
+        assert_eq!(zs.3, xs.3 * xs.3);
+    }
+}
